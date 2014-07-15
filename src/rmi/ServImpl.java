8228f1e558eb26a80acfecf6df01143220c8d4e6
a1097075 <a1097075@Einherjar>
2014-07-15 14:25:50 -0300

    Cliente

---
+++ src/rmi/ServImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rmi;
+
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author a1097075
+ */
+public class ServImpl implements InterfaceServ{
+
+    @Override
+    public void chamar(String nomeCliente, InterfaceCli interfaceCliente) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
