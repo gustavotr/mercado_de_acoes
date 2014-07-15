d78926cd71e5b93569fad89c3b8d8333ca2753e6
a1097075 <a1097075@Einherjar>
2014-07-15 15:24:02 -0300

    Servidor

--- src/rmi/servidor/Servidor.java
+++ src/rmi/servidor/Servidor.java
@@ -6,6 +6,14 @@
 
 package rmi.servidor;
 
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author a1097075
@@ -16,7 +24,17 @@ public class Servidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            Registry r = LocateRegistry.createRegistry(2024);         
+            ServImpl serv = new ServImpl();
+            r.bind("servidor", serv);
+        } catch (RemoteException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (AlreadyBoundException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
